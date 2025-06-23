dcae4cf5794dbf491cf1b76ecab62d766971de0b
reem022 <63267737+reem022@users.noreply.github.com>
2025-06-23 05:20:35 +0400

    drone

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Web Demo-0.1.0";
+const contentToCache = [
+    "Build/Exterior New BUILD.loader.js",
+    "Build/Exterior New BUILD.framework.js.unityweb",
+    "Build/Exterior New BUILD.data.unityweb",
+    "Build/Exterior New BUILD.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
